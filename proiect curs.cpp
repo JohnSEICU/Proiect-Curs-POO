f906c474279076ab052089f9c5a656b35cab76d9
TudorBo <103148933+TudorBo@users.noreply.github.com>
2023-01-16 00:05:26 +0200

    Update proiect curs.cpp
    Noul cod pentru proiect.

--- proiect curs.cpp
+++ proiect curs.cpp
@@ -1,93 +1,353 @@
-// file posible words
-// is valid
-// reshuffle function
-// starting point
-
 #include <iostream>
-#include <string.h>
-#include <stdlib.h>
-#include <fstream> // file reading
-#include <ctime> // timer for the player to write the word (1 min)
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <chrono>
 
-// litera mica - letter multiplier
-// litera mare - word multiplier
+int MAX_HEALTH = 3;
+const int GAME_DURATION_MINUTES = 60;
+const int QUESTION_DURATION_SECONDS = 120;
 
-// T 1 1 d 1 1 1 T 1 1 1 d 1 1 T
-// 1 D 1 1 1 t 1 1 1 t 1 1 1 D 1
-// 1 1 D 1 1 1 d 1 d 1 1 1 D 1 1
-// d 1 1 D 1 1 1 d 1 1 1 D 1 1 d
-// 1 1 1 1 D 1 1 1 1 1 D 1 1 1 1
-// 1 t 1 1 1 t 1 1 1 t 1 1 1 t 1
-// 1 1 d 1 1 1 d 1 d 1 1 1 d 1 1
-// T 1 1 d 1 1 1 S 1 1 1 d 1 1 T
-// 1 1 d 1 1 1 d 1 d 1 1 1 d 1 1
-// 1 t 1 1 1 t 1 1 1 t 1 1 1 t 1
-// 1 1 1 1 D 1 1 1 1 1 D 1 1 1 1
-// d 1 1 D 1 1 1 d 1 1 1 D 1 1 d
-// 1 1 D 1 1 1 d 1 d 1 1 1 D 1 1
-// 1 D 1 1 1 t 1 1 1 t 1 1 1 D 1
-// T 1 1 d 1 1 1 T 1 1 1 d 1 1 T
+const std::vector <int> path_begin = 
+{1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29};
+const std::vector <int> path_end = 
+{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 27, 28, 29, 30};
+const std::vector <std::string> path_diretions = 
+{"East", "North", "South", "East", "East", "North", "South", "West", "West", "North", "South", "East", "East", "East", "East", "East", "East", "South", "North", "South", "North", "East", "East", "East", "East", "South", "North", "East", "East", "East"};
 
-class Player
+class Room 
 {
-    public:
-        int points;
-        int pointCalculator(std::string word);
+    private:
+        
+        std::string question;
+        std::string answer;
+        std::string hint;
+        std::vector<std::string> exits;
+        bool is_completed;
+        bool is_end;
 
-        subclass - word
-            public:
-                std::string word;
-                boolean canBeUsed();
-                void returnPlayerPoints();
+    public:
+        //default constructor
+        Room() = default;
+        Room(std::string question, std::string answer, std::string hint, std::vector<std::string> exits, bool is_completed, bool is_end) : question(question), answer(answer), hint(hint), exits(exits), is_completed(is_completed), is_end(is_end) {}
+
+        //gettes
+        std::vector<std::string> getExits();
+        std::string getQuestion();
+        std::string getAnswer();
+        std::string getHint();
+        bool getIsCompleted();
+        bool getIsEnd();
+
+        //setters
+        void setExits(std::vector<std::string> exits);
+        void setQuestion(std::string question);
+        void setAnswer(std::string answer);
+        void setHint(std::string hint);
+        void setIsCompleted(bool is_completed);
+        void setIsEnd(bool is_end);
+
+        // void printRoom();
+
+        //default destructor
+        ~Room() = default;
+
+        friend class Map;
 };
 
-class Board
+
+//getters implementation
+std::vector<std::string> Room::getExits() 
+{
+    return exits;
+}
+
+std::string Room::getQuestion() 
+{
+    return question;
+}
+
+std::string Room::getAnswer() 
+{
+    return answer;
+}
+
+std::string Room::getHint() 
+{
+    return hint;
+}
+
+bool Room::getIsCompleted() 
+{
+    return is_completed;
+}
+
+bool Room::getIsEnd() 
+{
+    return is_end;
+}
+
+//setters implementation
+void Room::setExits(std::vector<std::string> exits) 
+{
+    this->exits = exits;
+}
+
+void Room::setQuestion(std::string question) 
+{
+    this->question = question;
+}
+
+void Room::setAnswer(std::string answer) 
+{
+    this->answer = answer;
+}
+
+void Room::setHint(std::string hint) 
+{
+    this->hint = hint;
+}
+
+void Room::setIsCompleted(bool is_completed) 
+{
+    this->is_completed = is_completed;
+}
+
+void Room::setIsEnd(bool is_end) 
+{
+    this->is_end = is_end;
+}
+
+// *print method
+// void Room::printRoom() 
+// {
+//     std::cout << "Question: " << question << std::endl;
+//     std::cout << "Answer: " << answer << std::endl;
+//     std::cout << "Hint: " << hint << std::endl;
+//     std::cout << "Exits: ";
+//     for(auto i : exits)
+//         std::cout << i << " ";
+//     std::cout << std::endl;
+//     std::cout << "Is completed: " << is_completed << std::endl;
+//     std::cout << "Is end: " << is_end << std::endl;
+// }
+
+//* operator<< overload for vector
+// std::ostream& operator<<(std::ostream& os, std::vector <std::string> exits)
+// {
+//     for(auto i : exits)
+//         std::cout << i << " ";
+
+//     return os;
+// }
+
+
+class Map
 {
     public:
-        std::string type;
-        int multiplier;
-        boolean is_word; // ???
+        bool validMove(std::string move, std::vector<std::string> exits);
+        void printRoom(Room room, int i);
+        bool answerQuestion(std::string answer, std::string correct_answer, int &health, std::string hint, int &try_count);
+        void showHint(std::string hint);
+        int getMove(std::vector<std::string> exits, int i);
+
 };
 
-class Letter
+bool Map::validMove(std::string move, std::vector<std::string> exits) 
 {
-    public:
-        int value_of_letter;
-        int count_of_letter;
-        std::string letter;
-};
+    auto it = find(exits.begin(), exits.end(), move);
+    for(auto i : exits)
+        if(it != exits.end())
+            return true;
+    return false;
+}
 
-void pointCalculator()
+void Map::printRoom(Room room, int i) 
 {
-    if(is_word == true)
+    std::string move;
+    std::string answer;
+    int try_count = 0;
+    if(room.is_completed == false)
     {
-        for(int i = 0 ; i < sizeof(word) ; i++)
-        {
-            sum = sum + litere.value_of_letter;
-        }
+        std::cout << "Room " << i + 1 << std::endl;
+        std::cout << "Question: " << room.question << std::endl;
+        std::cout << "Answer: ";
+        std::cin >> answer;
+        answerQuestion(answer, room.answer, MAX_HEALTH, room.hint, try_count);
+        room.is_completed = true;
 
-        points = points + sum*multiplier;
+        
+        if(answerQuestion(answer, room.answer, MAX_HEALTH, room.hint, try_count) == true)
+        {
+            std::cout << "Correct answer!" << std::endl;
+            room.is_completed = true;
+            std::cout << "Exits: ";
+            for(auto j : room.exits)
+                std::cout << j << " ";
+            std::cout << std::endl;
+
+
+            getMove(room.exits, i);
+        }
+        else
+        {   
+            do
+            {
+                std::cout << "Wrong answer!" << std::endl;
+                std::cout << "health: " << MAX_HEALTH << std::endl;
+                std::cout << "Answer: ";
+                std::cin >> answer;
+                // answerQuestion(answer, room.answer, MAX_HEALTH, room.hint, try_count);
+            } while (answerQuestion(answer, room.answer, MAX_HEALTH, room.hint, try_count) == false && MAX_HEALTH > 0);
+        }
+        
+    } 
+}
+
+bool Map::answerQuestion(std::string answer, std::string correct_answer, int &health, std::string hint, int &try_count) 
+{
+    if(answer == correct_answer)
+    {
+        return true;
     }
     else
     {
-        for(int i = 0 ; i < sizeof(word) ; i++)
+        if(try_count == 0)
         {
-            sum = sum + litere.value_of_letter*multiplier;
+            
+            showHint(hint);
+            try_count++;
+            return false;
+        }
+        else
+        {
+
+            std::cout << "You have no more hints!" << std::endl;
+            health--;
+            return false;
         }
 
-        points = points + sum;
     }
+}
+
+void Map::showHint(std::string hint) 
+{
+    std::cout << "Hint: " << hint << std::endl;
+}
+
+int Map::getMove(std::vector<std::string> exits, int i) 
+{   
+    int pos;
+
+    for(int k = 0; k < path_begin.size(); k++)
+    {
+        if(i == path_begin.at(k))
+        {
+            pos = k;
+        }
+    }
+
+    int new_room;
+    std::string move;
+    std::cout << "Move: ";
+    std::cin >> move;
+
+    if(validMove(move, exits) == false)
+    {
+        do
+        {
+            std::cout << "Invalid move!" << std::endl;
+            std::cout << "Move: ";
+            std::cin >> move;
+        } while(validMove(move, exits) == false);
+    }
+
+    for(int j = 0; j < path_begin.size(); j++)
+    {
+        if(move == path_diretions.at(j) && pos == path_begin.at(j))
+        {
+            new_room = path_end.at(j);
+        }
+    }
+
+    i = new_room - 1;
+
+    return i;
 }
 
 int main()
 {
-    Player player1;
-    Player player2;
-    Player player3;
-    Player player4;
+    Map map;
 
-    Board board[15][15]; // file reading - csv
+    std::vector<Room> rooms;
+    std::string question;
+    std::string answer;
+    std::string hint;
+    std::vector <std::string> exits;
+    bool is_completed;
+    bool is_end;
+    
+    std::string aux;
+    std::string row;
+    std::ifstream fin;
+    int room_count = -1;
 
-    Letter litere[26]; // file reading - csv
-    int total_letter_count;
+    //filereading in vector
+    fin.open("questions.csv");
+
+    while(std::getline(fin, row))
+    {
+        std::stringstream sin(row);
+        room_count++;
+
+        std::getline(sin, aux, ',');
+        question = aux;
+
+        std::getline(sin, aux, ',');
+        answer = aux;
+
+        std::getline(sin, aux, ',');
+        hint = aux;
+
+        std::getline(sin, aux, ',');
+        std::stringstream sin2(aux);
+        while (std::getline(sin2, aux, ' ')) 
+        {
+            exits.push_back(aux);
+        }
+
+        std::getline(sin, aux, ',');
+        is_completed = aux == "true" ? true : false;
+
+        std::getline(sin, aux, ',');
+        is_end = aux == "true" ? true : false;
+
+        rooms.push_back(Room(question, answer, hint, exits, is_completed, is_end));
+
+        exits.clear();
+    }
+
+    fin.close();
+
+    //game loop
+    //! TREBUIE SA RETURNZE POZITIA CAMEREI URMATOARE
+    for(int i = 0; i < rooms.size() && MAX_HEALTH > 0; i++)
+    {
+        map.printRoom(rooms[i], i);
+        std::cout << rooms[i].getIsCompleted() << std::endl;
+
+        if(rooms[i].getIsEnd() == true)
+        {
+            std::cout << "You won!" << std::endl;
+            return 0;
+        }
+    }
+
+    std::cout << "You ran out of lives!" << std::endl;
+
+
+    return 0;
 }
+
